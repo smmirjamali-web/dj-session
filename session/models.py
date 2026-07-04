02cad3476ee50f8e260e443b6cd6494e4e4dbef1
Mahdi-Mirjamali <smmirjamali@google.com>
2026-07-05 00:24:15 +0330

    Add custom session model

--- session/models.py
+++ session/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Session(models.Model):
+    session_key = models.CharField(
+        max_length=64, 
+        unique=True
+        )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE
+        )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+        )
+    expire_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user} - {self.session_key[:6]}"
