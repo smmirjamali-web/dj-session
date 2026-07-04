7c2fd137a7e02afe6e11023fab1092ab9000a149
Mahdi-Mirjamali <smmirjamali@google.com>
2026-07-04 18:45:15 +0330

    Configure PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'dj_session',
+        'USER': 'mehdi',
+        'PASSWORD': '1234',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
